69c55fab500cef81dff807d77ab8e525d11be39a
Luckyaremu <aremu.baba70@gmail.com>
2020-05-21 10:43:25 +0100

    friend request initiated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
+  def send_friendship_request
+    f = Friendship.where(user_id: params[:id], friend_id: current_user.id)
+        if f.exist?
+          f.update_all(confirmed:true)
+          redirect_to users_path
+          return
+        end
+        r = Friendship.where(user_id: current_user.id, friend_id: params[:id]).exists?
+        if !r
+        friend = Friendship.new(user_id: current_user.id, friend_id: params[:id],confirmed:false)
+        friend.save
+        else
+          friend = Friendship.where(user_id: current_user.id, friend_id: params[:id]).select('id')
+          Friendship.destroy(friend.ids)
+        end
+        redirect_to users_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   devise_for :users
+  
+  resources :users, only: [:index, :show,:destroy] do
+    member do
+      match 'send_friendship_request' => 'users#send_friendship_request', via: [:get]
+    end
+  end
 
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :create] do

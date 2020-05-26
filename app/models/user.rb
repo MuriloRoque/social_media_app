c7b4ed5e967f1cc288a2c4edd50cbbce6d80e6d5
Murilo Roque <muriloengqui@gmail.com>
2020-05-26 09:36:50 -0300

    Add new methods and relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,12 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :inverse_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friend_requests, through: :inverse_friendships
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
+  has_many :friends, through: :confirmed_friendships
 
   def mutual_friends(current_user, user)
     count = []
@@ -26,5 +31,9 @@ class User < ApplicationRecord
   def friends?(id)
     Friendship.where(user_id: self.id, friend_id: id, confirmed: true).exists? ||
       Friendship.where(friend_id: self.id, user_id: id, confirmed: true).exists?
+  end
+
+  def friends_and_own_posts
+    Post.where(user: (self.friends + self))
   end
 end
